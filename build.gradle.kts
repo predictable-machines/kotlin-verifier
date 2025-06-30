e9f16223235b6c6779f58d12c8af76988820662c
Raúl Raja Martínez <raulraja@gmail.com>
2025-07-01 01:22:05 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("multiplatform") version "2.1.20" apply false
+    kotlin("jvm") version "2.1.20" apply false
+    id("com.github.gmazzo.buildconfig") version "5.6.5"
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.3" apply false
+}
+
+allprojects {
+    group = "org.jetbrains.kotlin.compiler.plugin.template"
+    version = "0.1.0-SNAPSHOT"
+}
---
+++ compiler-plugin/build.gradle.kts
@@ -0,0 +1,105 @@
+plugins {
+    kotlin("jvm")
+    `java-test-fixtures`
+    id("com.github.gmazzo.buildconfig")
+    idea
+}
+
+sourceSets {
+    main {
+        java.setSrcDirs(listOf("src"))
+        resources.setSrcDirs(listOf("resources"))
+    }
+    testFixtures {
+        java.setSrcDirs(listOf("test-fixtures"))
+    }
+    test {
+        java.setSrcDirs(listOf("test", "test-gen"))
+        resources.setSrcDirs(listOf("testData"))
+    }
+}
+
+idea {
+    module.generatedSourceDirs.add(projectDir.resolve("test-gen"))
+}
+
+val annotationsRuntimeClasspath: Configuration by configurations.creating { isTransitive = false }
+
+dependencies {
+    compileOnly(kotlin("compiler"))
+
+    testFixturesApi(kotlin("test-junit5"))
+    testFixturesApi(kotlin("compiler-internal-test-framework"))
+    testFixturesApi(kotlin("compiler"))
+
+    annotationsRuntimeClasspath(project(":plugin-annotations"))
+
+    // Dependencies required to run the internal test framework.
+    testRuntimeOnly("junit:junit:4.13.2")
+    testRuntimeOnly(kotlin("reflect"))
+    testRuntimeOnly(kotlin("test"))
+    testRuntimeOnly(kotlin("script-runtime"))
+    testRuntimeOnly(kotlin("annotations-jvm"))
+}
+
+buildConfig {
+    useKotlinOutput {
+        internalVisibility = true
+    }
+
+    packageName(group.toString())
+    buildConfigField("String", "KOTLIN_PLUGIN_ID", "\"${rootProject.group}\"")
+}
+
+tasks.test {
+    dependsOn(annotationsRuntimeClasspath)
+
+    useJUnitPlatform()
+    workingDir = rootDir
+
+    systemProperty("annotationsRuntime.classpath", annotationsRuntimeClasspath.asPath)
+
+    // Properties required to run the internal test framework.
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-stdlib", "kotlin-stdlib")
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-stdlib-jdk8", "kotlin-stdlib-jdk8")
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-reflect", "kotlin-reflect")
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-test", "kotlin-test")
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-script-runtime", "kotlin-script-runtime")
+    setLibraryProperty("org.jetbrains.kotlin.test.kotlin-annotations-jvm", "kotlin-annotations-jvm")
+
+    systemProperty("idea.ignore.disabled.plugins", "true")
+    systemProperty("idea.home.path", rootDir)
+}
+
+kotlin {
+    compilerOptions {
+        optIn.add("org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi")
+        optIn.add("org.jetbrains.kotlin.ir.symbols.UnsafeDuringIrConstructionAPI")
+    }
+}
+
+val generateTests by tasks.registering(JavaExec::class) {
+    inputs.dir(layout.projectDirectory.dir("testData"))
+        .withPropertyName("testData")
+        .withPathSensitivity(PathSensitivity.RELATIVE)
+    outputs.dir(layout.projectDirectory.dir("test-gen"))
+        .withPropertyName("generatedTests")
+
+    classpath = sourceSets.testFixtures.get().runtimeClasspath
+    mainClass.set("org.jetbrains.kotlin.compiler.plugin.template.GenerateTestsKt")
+    workingDir = rootDir
+}
+
+tasks.compileTestKotlin {
+    dependsOn(generateTests)
+}
+
+fun Test.setLibraryProperty(propName: String, jarName: String) {
+    val path = project.configurations
+        .testRuntimeClasspath.get()
+        .files
+        .find { """$jarName-\d.*jar""".toRegex().matches(it.name) }
+        ?.absolutePath
+        ?: return
+    systemProperty(propName, path)
+}
